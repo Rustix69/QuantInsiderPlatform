00a234f108370cd7f515ccbce482e87670ada396
ANIR1604 <competitivecoder69@gmail.com>
2025-05-04 15:20:03 +0530

    Event added conference iiqc

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -21,6 +21,58 @@ export function TimelineDemo() {
 
   const data = [
     {
+      title: "QuantVision",
+      dates: "22nd May 2025",
+      content: (
+        <div>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-sm md:text-lg font-bold mb-8">
+            Sponsor at QuantVision 2025: Fordham’s Quantitative Conference & Data Summit
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <img
+              src="./eventsection/QuantVision/iiqc01.png"
+              alt="hero template"
+              width="100%"
+              height="100%"
+              className="rounded-lg object-cover h-28 md:h-44 lg:h-72 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+            <img
+              src="./eventsection/QuantVision/iiqc02.png"
+              alt="hero template"
+              width="100%"
+              height="100%"
+              className="rounded-lg object-cover h-28 md:h-44 lg:h-72 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+          </div>
+        </div>
+      )
+    },
+    {
+      title: "IIQC",
+      dates: "16th May 2025",
+      content: (
+        <div>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-sm md:text-lg font-bold mb-8">
+            Education Partners at LAQSA  Indian Institutional Quant Conference – Bengaluru Edition 2025
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <img
+              src="./eventsection/IIQC/img01.png"
+              alt="hero template"
+              width="100%"
+              height="100%"
+              className="rounded-lg object-cover h-28 md:h-44 lg:h-72 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+            <img
+              src="./eventsection/IIQC/img02.png"
+              alt="hero template"
+              width="100%"
+              height="100%"
+              className="rounded-lg object-cover h-28 md:h-44 lg:h-72 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+          </div>
+        </div>
+      )
+    },
+    {
       title: "NIT Tirchy",
       dates: "29 - 30 March  2025",
       content: (
