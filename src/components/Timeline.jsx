2a1ed687a0ec0a595a6f83bae63e7f680dc4ebba
ANIR1604 <competitivecoder69@gmail.com>
2025-05-25 16:20:40 +0530

    Added teh new images fordham

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -48,22 +48,22 @@ export function TimelineDemo() {
     },
     {
       title: "QuantVision",
-      dates: "22nd May 2025 (Upcoming)",
+      dates: "22nd May 2025",
       content: (
         <div>
           <p
             className="text-neutral-800 dark:text-neutral-200 text-sm md:text-lg font-bold mb-8">
-            Sponsor at QuantVision 2025: Fordham's Quantitative Conference & Data Summit, Fordham University, Gabelli School of Business, Lincoln Center Campus <a href="https://www.rebellionresearch.com/quant-insider-to-sponsor-quantvision-2025-fordhams-quantitative-conference-data-summit" target="_blank" className="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300 underline">Visit Now</a>
+            Sponsor at QuantVision 2025: Fordham's Quantitative Conference & Data Summit, Fordham University, Gabelli School of Business, Lincoln Center Campus
           </p>
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="./eventsection/QuantVision/iiqc01.png"
+              src="./eventsection/QuantVision/img01.jpg"
               alt="hero template"
               width="100%"
               height="100%"
               className="rounded-lg object-cover h-28 md:h-44 lg:h-72 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
             <img
-              src="./eventsection/QuantVision/iiqc02.png"
+              src="./eventsection/QuantVision/img02.jpg"
               alt="hero template"
               width="100%"
               height="100%"
