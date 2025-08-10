1414880f438b904cf2c068b98d5f1edff0579a4d
ANIR1604 <competitivecoder69@gmail.com>
2025-08-10 22:35:25 +0530

    Corneel Done

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -21,6 +21,32 @@ export function TimelineDemo() {
 
   const data = [
     {
+      title: "Cornell University",
+      dates: "19 Sep 2025 (Upcoming)",
+      content: (
+        <div>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-sm md:text-lg font-bold mb-8">
+            Quant Insider To Sponsor Cornell Financial Engineering Manhattan 2025 Future of Finance & Ai Conference <a href="https://www.rebellionresearch.com/quant-insider-to-sponsor-cornell-financial-engineering-manhattan-2025-future-of-finance-ai-conference" target="_blank" className="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300 underline"> Visit Now</a>
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <img
+              src="./eventsection/Cornell/img01.png"
+              alt="Cornell event"
+              width="100%"
+              height="100%"
+              className="rounded-lg object-cover h-28 md:h-44 lg:h-72 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+            <img
+              src="./eventsection/Cornell/img02.png"
+              alt="Cornell event"
+              width="100%"
+              height="100%"
+              className="rounded-lg object-cover h-28 md:h-44 lg:h-72 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+          </div>
+        </div>
+      )
+    },
+    {
       title: "Palermo, Italy",
       dates: "24 - 26 Sep 2025 (Upcoming)",
       content: (
