8ecb07606e6ef56d9a0e6aa7eae3df19004451bb
ANIR1604 <competitivecoder69@gmail.com>
2025-10-27 10:44:41 +0530

    Dummy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000, // Increase the limit (1000 kB)
-  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
+
+
